09f73aa64d7f824fc6693e69f90b648c2457164b
Bavlardhan Pariahr <balvardhan80@gmail.com>
2021-05-17 13:19:14 +0530

    now carousel is fully functioning

--- README.md
+++ README.md
@@ -3,4 +3,8 @@ This is not a personal project or any site build up, i am just making a clone of
 I am new in this feild and i have learned HTML, CSS and Javascript pretty much and now i decided to create clones of random websites for practice.
 So i am a noob right now and if i am doing any mistake anywhere in the project then you can tell me.
 email - balvardhan80@gmail.com
+
+Maybe this clone is not exactly like the original one because some of the properties like margin, padding, font-weight, font-size and etc are slightly different from original. Beacause i am not copy pasting the origianl site.
+
+
 # jetbrains-clone
--- app.js
+++ app.js
@@ -5,22 +5,39 @@ const slide_number = document.querySelector(".carousel-count");
 var count = 1;
 carousel.style.left = 0;
 var going_back = false;
+left_arrow.style.opacity = "0.7"
 
 var move_backward = function(){
     carousel.style.left = parseInt(carousel.style.left) + 100 + "%";
     count--;
     slide_number.innerHTML = `${count}/3`
+    if(count == 1){
+        left_arrow.style.opacity = "0.7";
+        right_arrow.style.opacity = "1";
+    }
+    else{
+        left_arrow.style.opacity = "1";
+        right_arrow.style.opacity = "1";
+        
+    }
 }
 
 var move_forward = function(){
     carousel.style.left = parseInt(carousel.style.left) - 100 + "%";
     count++;
     slide_number.innerHTML = `${count}/3`
+    if(count == 3){
+        right_arrow.style.opacity = "0.7";
+        left_arrow.style.opacity = "1";
+    }
+    else{
+        right_arrow.style.opacity = "1";
+        left_arrow.style.opacity = "1";
+    }
 }
 
 var sliding = setInterval(() => {
     if(count < 3){
-        console.log(going_back)
         if(going_back && count!==1){
             move_backward();
         }
@@ -39,7 +56,7 @@ right_arrow.addEventListener("click",()=>{
     if(!(count == 3)){
         clearInterval(sliding);
         move_forward();
-    };
+    }
 });
 
 left_arrow.addEventListener("click",()=>{
